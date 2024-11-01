bc0248d83025b0b7af8a5af1c0919af921466af8
Venegas <dvenegas7@cps.k12.il.us>
2024-11-01 12:29:10 -0500

    first challenge

--- Challenge_assigment.py
+++ Challenge_assigment.py
@@ -1,3 +1,6 @@
+# Problem 1 
+
+
 #this is my input asking for the score
 scores=int(input("What was your score:"))
 
@@ -16,3 +19,20 @@ else: # if the code is less tan 0 this prints out
      print(" Your score cant be negative")
      
    
+#Problem 2
+
+start=int(input("Please enter the starting number:"))#asks the user for the start number
+end=int(input("Please enter the ending number:"))# asks the user for the end number
+newend=(end + 1)# adds one to the end number because when put in a range the range takes the number before the one you entered
+
+for i in range(start, newend): #print the numbers in between the start and stop 
+    print(i)
+
+
+for i in range(start, newend): # print the numbers in between the start and stop only if the are even and greater than 10
+    if (i % 2 == 0) and i > 10:
+        print(i)
+
+for i in range(start, newend): #print the numbers in between the start and stop only if the are odd and less than 20
+    if (i % 2 ) and i < 20:
+        print(i)
